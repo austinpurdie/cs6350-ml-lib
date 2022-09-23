edbfb6a0d1a40437cff306604cc5b629a14b839b
austinpurdie <101536676+austinpurdie@users.noreply.github.com>
2022-09-22 23:49:13 -0600

    Added a joke :)

--- DecisionTree/run.sh
+++ DecisionTree/run.sh
@@ -5,6 +5,8 @@ python car.py
 
 echo "Running bank-prob3a.py..."
 echo "This will probably take a while..."
+echo "Here's my favorite joke while you wait:"
+echo "Three men are sitting in a boat. Each has a cigarette but no one has a lighter, so one of the men throws his cigarette overboard and the whole boat becomes a cigarette lighter."
 python bank-prob3a.py
 
 echo "Running bank-prob3b.py..."
