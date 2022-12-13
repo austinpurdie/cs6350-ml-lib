ef8d3621500d9e5f43d36ec33c60c8e57c8fab01
austinpurdie <101536676+austinpurdie@users.noreply.github.com>
2022-12-12 20:01:52 -0700

    Update hw5 shell script

--- run-hw5.sh
+++ run-hw5.sh
@@ -6,6 +6,6 @@ pip install pandas
 echo "Will sleep five minutes after execution so you can view the results..."
 
 echo "Running hw5-prob2bc.py..."
-python NeuralNetworks/hw5-prob-2bc.py
+python NeuralNetworks/hw5-prob-2abc.py
 
 sleep 300
